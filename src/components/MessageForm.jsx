fee688d057aed29079cb0d3820eaed1b3eb70834
anshu0x <anshusharma6327@gmail.com>
2022-10-11 15:57:33 +0530

    svg removed

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import sendBtn from "../assets/images/send.svg";
 const MessageForm = ({
   msg,
   sendMsg,
@@ -47,7 +46,7 @@ const MessageForm = ({
           placeholder="Message"
         />
         <Button type="submit" variant="success send-message">
-          <img width={"25"} src={sendBtn} />
+          <span class="material-symbols-outlined">send</span>
         </Button>
       </Form>
     </footer>
