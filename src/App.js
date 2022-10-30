f0e9934bac2c78c4a7137da844dbce62147259e8
anshu0x <anshusharma6327@gmail.com>
2022-10-30 22:12:41 +0530

    database added for messages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-import Sound from "./assets/Sounds/notification.mp3";
 import LoginPage from "./components/LoginPage";
 import SignUp from "./components/SignUp";
 import Home from "./Home";
 import { Routes, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 import VerifyOtp from "./components/VerifyOtp";
-const socket = io("https://scoket.azurewebsites.net");
+const socket = io("http://localhost:5000");
+// const socket = io("https://scoket.azurewebsites.net");
 function App() {
   const [msg, setMsg] = useState("");
   const [room, setRoom] = useState("");
@@ -16,7 +16,6 @@ function App() {
   let UserName = localStorage?.getItem("name") ?? "";
   const [Usrname, setmUserName] = useState(UserName);
   const [userCount, setUserCount] = useState(0);
-  const audio = new Audio(Sound);
   const sendMsg = async () => {
     await socket.emit("send_msg", { msg, Usrname });
     setMsg("");
@@ -25,16 +24,17 @@ function App() {
     await socket.emit("send_pvt_msg", { msg, room, Usrname });
     setMsg("");
   };
+  console.log(msgRec);
   useEffect(() => {
+    socket.on("all_messages", async (data) => {
+      setmsgRec(data);
+    });
     socket.on("received_msg", async (data) => {
-      // this will braodcast message to other open another window to see result
-      await setmsgRec((prev) => [...prev, data]);
-      await audio.play();
+      setmsgRec((prev) => [...prev, data]);
     });
     socket.on("pvt_received_msg", async (data) => {
       // this will braodcast message to other open another window to see result
       await setpvtmsg((prev) => [...prev, data]);
-      await audio.play();
     });
     socket.on("userCount", (data) => {
       setUserCount(data);
